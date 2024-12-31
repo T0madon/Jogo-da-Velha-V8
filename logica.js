2c0c3a7c5e79fb8adfe3b0fdf4ffdad042b0ff00
T0madon <joao.daciuk@gmail.com>
2024-12-31 13:04:55 -0300

    win-regions

--- logica.js
+++ logica.js
@@ -3,6 +3,68 @@ let vBoard = [];
 let lastwin = "player2";
 let turnPlayer = "";
 
+function verificaWin() {
+  const winRegions = [];
+
+  if (
+    vBoard[0][0] &&
+    vBoard[0][0] === vBoard[0][1] &&
+    vBoard[0][0] === vBoard[0][2]
+  )
+    winRegions.push("0.0", "0.1", "0.2"); // Primeira linha
+
+  if (
+    vBoard[1][0] &&
+    vBoard[1][0] === vBoard[1][1] &&
+    vBoard[1][0] === vBoard[1][2]
+  )
+    winRegions.push("1.0", "1.1", "1.2"); // Segunda linha
+
+  if (
+    vBoard[2][0] &&
+    vBoard[2][0] === vBoard[2][1] &&
+    vBoard[2][0] === vBoard[2][2]
+  )
+    winRegions.push("2.0", "2.1", "2.2"); // Terceira linha
+
+  if (
+    vBoard[0][0] &&
+    vBoard[0][0] === vBoard[1][0] &&
+    vBoard[0][0] === vBoard[2][0]
+  )
+    winRegions.push("0.0", "1.0", "2.0"); // Primeira coluna
+
+  if (
+    vBoard[0][1] &&
+    vBoard[0][1] === vBoard[1][1] &&
+    vBoard[0][1] === vBoard[2][1]
+  )
+    winRegions.push("0.1", "1.1", "2.1"); // Segunda coluna
+
+  if (
+    vBoard[0][2] &&
+    vBoard[0][2] === vBoard[1][2] &&
+    vBoard[0][2] === vBoard[2][2]
+  )
+    winRegions.push("0.2", "1.2", "2.2"); // Terceira coluna
+
+  if (
+    vBoard[0][0] &&
+    vBoard[0][0] === vBoard[1][1] &&
+    vBoard[0][0] === vBoard[2][2]
+  )
+    winRegions.push("0.0", "1.1", "2.2"); // Diagonal principal
+
+  if (
+    vBoard[0][2] &&
+    vBoard[0][2] === vBoard[1][1] &&
+    vBoard[0][2] === vBoard[2][0]
+  )
+    winRegions.push("0.2", "1.1", "2.0"); // Diagonal secundária
+
+  return winRegions;
+}
+
 function atualizaTitulo() {
   const playerInput = document.getElementById(turnPlayer);
   document.getElementById("turnPlayer").innerText = playerInput.value;
@@ -26,6 +88,11 @@ function inicializar() {
   });
 }
 
+function desabilita(elemento) {
+  elemento.style.cursor = "default";
+  elemento.removeEventListener("click", cliqueNaVelha);
+}
+
 function cliqueNaVelha(ev) {
   const quadrado = ev.currentTarget;
   const regiao = quadrado.dataset.region;
@@ -36,10 +103,14 @@ function cliqueNaVelha(ev) {
   if (turnPlayer === "player1") {
     quadrado.innerText = "X";
     vBoard[linha][coluna] = "X";
+    turnPlayer = "player2";
   } else {
     quadrado.innerText = "O";
     vBoard[linha][coluna] = "O";
+    turnPlayer = "player1";
   }
+
+  desabilita(quadrado);
 }
 
 document.getElementById("begin").addEventListener("click", inicializar);
